4c654933314998fcaf705c7880b75f48496bdd68
newazsharifsaikot <saikotkhan44@gmail.com>
2022-01-01 02:01:25 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+//scroll reveal
+window.scrl = ScrollReveal()
+scrl.reveal('.container', {
+    duration:1000
+})
+scrl.reveal('.container-fluid', {
+    duration:1000
+})
+scrl.reveal('.row', {
+    duration:1000
+})
+scrl.reveal('.footer p',{
+    duration:1000
+});
+
